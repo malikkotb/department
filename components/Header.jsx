9927a34ce84bcb495780f6b6763dd1247f6d7994
malikkotb <malikkotb@icloud.com>
2024-08-07 18:40:26 +0200

    setup

---
+++ components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="w-full h-24 borderr flex px-16 py-8 justify-between items-center">
+      <div className="uppercase">Basic/Dept</div>
+      <div className="flex items-center justify-between gap-12">
+        <div>Menu</div>
+        <div>Carreer</div>
+        <div>Work</div>
+        <div>Carreer</div>
+      </div>
+      <div>Menu</div>
+    </div>
+  );
+}
