01d8ea84d91765805f1295e023b846017a15a816
malikkotb <malikkotb@icloud.com>
2024-08-09 22:34:43 +0200

    setup

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div className="w-full h-24 borderr flex px-16 py-8 justify-between items-center">
+    <div className="w-full h-24 borderr flex px-16 py-8 items-center">
       <div className="uppercase">Basic/Dept</div>
-      <div className="flex items-center justify-between gap-12">
+      <div className="flex items-center justify-between gap-12 m-auto">
+        <div>Carreer</div>
         <div>Menu</div>
         <div>Carreer</div>
         <div>Work</div>
---
+++ components/HeaderLink.jsx
@@ -0,0 +1,13 @@
+import motion from 'framer-motion'
+
+export default function HeaderLink({ title}) {
+  return (
+    <div>
+        {/* TODO: on hover animation */}
+        {title}
+        <motion.div >
+
+        </motion.div>
+    </div>
+  )
+}
