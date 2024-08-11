04cd69b90a12a4421e426351676e3ed886427ad5
malikkotb <malikkotb@icloud.com>
2024-08-11 21:27:19 +0200

    video

---
+++ components/VideoHome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function VideoHome() {
+  return (
+    <div>
+        <video src="https://www.youtube.com/watch?v=JGwWNGJdvx8" controls></video>
+    </div>
+  )
+}
