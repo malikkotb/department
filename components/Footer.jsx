52ddfc563434023f42463244e0938588cbcb0696
malikkotb <malikkotb@icloud.com>
2024-08-13 21:59:59 +0200

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,20 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div>
-      <div style={styles.container}>
-        <p>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
-        <nav style={styles.nav}>
-          <a href="/about" style={styles.link}>
-            About Us
-          </a>
-          <a href="/contact" style={styles.link}>
-            Contact
-          </a>
-          <a href="/privacy" style={styles.link}>
-            Privacy Policy
-          </a>
-        </nav>
-      </div>
+      
     </div>
   );
 }
