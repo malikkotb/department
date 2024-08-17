db574ef741b332eff0cbc3e176f79bf2a89e5dd8
malikkotb <malikkotb@icloud.com>
2024-08-17 21:39:59 +0200

    1

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div>
-      
-    </div>
+      {images.map((src, index) => (
+      <div key={index}>
+        <div
+          ref={addToImageRefs}
+          onMouseEnter={() => handleHover(1.2, index)}
+          onMouseLeave={() => handleHover(1, index)}
+          className="w-[20vw] h-[40vh] relative">
+            <Image src={src} fill style={{ objectFit: "cover" }} alt="run" />
+        </div>  
+    </div>)}
   );
 }
