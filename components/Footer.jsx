0b56275ea87a054ca64c61dc983442655bd5d99e
malikkotb <malikkotb@icloud.com>
2024-08-17 21:40:24 +0200

    1

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,15 +2,21 @@ import React from "react";
 
 export default function Footer() {
   return (
-      {images.map((src, index) => (
-      <div key={index}>
-        <div
-          ref={addToImageRefs}
-          onMouseEnter={() => handleHover(1.2, index)}
-          onMouseLeave={() => handleHover(1, index)}
-          className="w-[20vw] h-[40vh] relative">
-            <Image src={src} fill style={{ objectFit: "cover" }} alt="run" />
-        </div>  
-    </div>)}
+    <div>
+      <div style={styles.container}>
+        <p>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
+        <nav style={styles.nav}>
+          <a href="/about" style={styles.link}>
+            About Us
+          </a>
+          <a href="/contact" style={styles.link}>
+            Contact
+          </a>
+          <a href="/privacy" style={styles.link}>
+            Privacy Policy
+          </a>
+        </nav>
+      </div>
+    </div>
   );
 }
