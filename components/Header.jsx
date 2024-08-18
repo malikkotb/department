fa6f9bc66c6aa70075019424bf8eb881fcc7e278
malikkotb <malikkotb@icloud.com>
2024-08-18 10:26:08 +0200

    hover links

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+gsap.registerPlugin(useGSAP);
 
 export default function Header() {
+  useGSAP(() => {});
+
   return (
     <div className="w-full h-24 borderr flex px-16 py-8 items-center">
       <div className="uppercase">Basic/Dept</div>
       <div className="flex items-center justify-between gap-12 m-auto">
-        <div>Carreer</div>
-        <div>Menu</div>
-        <div>Carreer</div>
-        <div>Work</div>
-        <div>Carreer</div>
+        <Link href={"/"} className="headerLink">
+          Carreer
+        </Link>
+        <Link href={"/"} className="headerLink">
+          Menu
+        </Link>
+        <Link href={"/"} className="headerLink">
+          Carreer
+        </Link>
+        <Link href={"/"} className="headerLink">
+          Work
+        </Link>
+        <Link href={"/"} className="headerLink">
+          Carreer
+        </Link>
       </div>
       <div>Menu</div>
     </div>
