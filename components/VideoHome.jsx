b2fcd50b2ddef169505db593b92a2e80fc549cfa
malikkotb <malikkotb@icloud.com>
2024-08-22 16:49:31 +0200

    custom cursor

--- components/VideoHome.jsx
+++ components/VideoHome.jsx
@@ -2,36 +2,59 @@
 import { useWindowSize } from "@uidotdev/usehooks";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 export default function VideoHome() {
   const cursorRef = useRef(null);
   const sectionRef = useRef(null);
   const size = useWindowSize();
+  const [isHovered, setIsHovered] = useState(false);
+  const [position, setPosition] = useState({ top: 0, left: 0 });
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+    setPosition({ top: 450, left: 450 });
+  };
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+    setPosition({ top: 0, left: 0 });
+  };
 
   useGSAP(() => {
     const onMouseMove = (event) => {
       const { clientX, clientY } = event;
-      console.log(clientX, clientY);
-      gsap.set(cursorRef.current, { x: clientX - clientX / 2, y: clientY - clientY / 2 });
+      gsap.set(cursorRef.current, { x: clientX, y: clientY, opacity: 1, duration: 1 });
+    };
+
+    const onMouseLeave = (event) => {
+      gsap.set(cursorRef.current, { x: size.width / 2, y: size.height / 2 });
     };
 
     sectionRef.current.addEventListener("mousemove", onMouseMove);
+    sectionRef.current.addEventListener("mouseout", onMouseLeave);
 
     // TODO: cursor also has to move with scroll otherwise its weird
     // TODO: move cursor back to middle of screen once mouse is off this section / or on header
   });
   return (
-    <section ref={sectionRef} className="h-screen w-screen relative flex">
+    <section
+      ref={sectionRef}
+      className="h-screen w-screen relative flex"
+      onMouseEnter={handleMouseEnter}
+      // onMouseLeave={handleMouseLeave}
+    >
       {/* Custom Cursor */}
       <div
         ref={cursorRef}
         id="custom-cursor"
         style={{
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
+          top: `${position.top}px`,
+          left: `${position.left}px`,
+          opacity: 0, // Initially hidden
         }}
-        className="absolute h-24 w-24 rounded-full pointer-events-none z-10 p-2 flex items-center justify-center bg-white"
+        className={`${
+          isHovered ? "absolute" : "fixed"
+        } h-24 w-24 rounded-full pointer-events-none z-10 p-2 flex items-center justify-center bg-white`}
       >
         <div className="cursor-text text-sm leading-tight text-center font-semibold text-black uppercase tracking-wide">
           Watch Reel
