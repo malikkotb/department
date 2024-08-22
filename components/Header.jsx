ce63ac42621f7398f8735d6c1df89d28f0c36980
malikkotb <malikkotb@icloud.com>
2024-08-22 11:09:25 +0200

    custom cursor

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
   // scroll away when scrolling down and scroll back when scrolling up
 
   return (
-    <div className="w-full h-24 bg-transparent absolute borderr flex px-16 py-8 items-center">
+    <div className="w-full h-24 bg-transparent borderr flex px-16 py-8 items-center">
       <div className="uppercase">Basic/Dept</div>
       <div className="flex items-center justify-between gap-12 m-auto">
         <Link href={"/"} className="headerLink">
--- components/VideoHome.jsx
+++ components/VideoHome.jsx
@@ -1,11 +1,36 @@
-
-import Cursor from "./Cursor";
-
+"use client";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
 export default function VideoHome() {
-  // TODO: custom cursor
+  const cursorRef = useRef(null);
+  const textRef = useRef(null);
+
+  useGSAP(() => {
+    const onMouseMove = (event) => {
+      const { clientX, clientY } = event;
+      gsap.set(cursorRef.current, { x: clientX, y: clientY });
+    };
+
+    // TODO: instead of adding the eventListener to the document, add it to this page only or the
+    // parent component
+    document.addEventListener("mousemove", onMouseMove);
+  });
   return (
-    <section className="h-screen borderr bg-yellow-300">
-      <Cursor />
+    <section className="h-screen cursor-none borderr bg-blue-300">
+      {/* Custom Cursor */}
+      <div
+        ref={cursorRef}
+        id="custom-cursor"
+        className="fixed top-0 left-0 h-24 w-24 rounded-full pointer-events-none z-50 p-2 flex items-center justify-center bg-white"
+      >
+        <div
+          ref={textRef}
+          className="cursor-text text-xs text-center font-semibold text-black uppercase tracking-wide"
+        >
+          Watch Reel
+        </div>
+      </div>
 
       {/* <video src="https://www.youtube.com/watch?v=JGwWNGJdvx8" controls></video> */}
     </section>
