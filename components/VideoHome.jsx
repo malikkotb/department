672904dff9332fc175a2226f58e1cdb415737766
malikkotb <malikkotb@icloud.com>
2024-08-22 10:41:35 +0200

    custom cursor

--- components/VideoHome.jsx
+++ components/VideoHome.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+
+import Cursor from "./Cursor";
 
 export default function VideoHome() {
   // TODO: custom cursor
   return (
-    <section className="h-screen borderr cursor-none bg-yellow-300">
-      <div className="custom-cursor"></div>
+    <section className="h-screen borderr bg-yellow-300">
+      <Cursor />
+
       {/* <video src="https://www.youtube.com/watch?v=JGwWNGJdvx8" controls></video> */}
     </section>
   );
